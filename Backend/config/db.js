d587e70e833ca812b62c603a2c418997d1411c87
Ayush Kumar <ayush129kumar@gmail.com>
2025-01-18 10:11:56 +0530

    updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,6 +5,18 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database successfully.');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle database client:', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
